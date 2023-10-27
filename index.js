1fa8af963b37b2dd5921345395169fbe1307d7bf
Anton Khassanov <marioanton9@gmail.com>
2023-10-27 16:58:51 -0400

    creates cats file

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push (name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function appendCat(name){
+    const copy = cats.slice()
+    copy.push(name) 
+    return copy 
+}
+
+function removeLastCat(name){
+    const copy = cats.slice()
+    copy.pop(name)
+    return copy
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function prependCat(name){
+    const copy = cats.slice()
+    copy.unshift(name)
+    return copy
+}
+
+function removeFirstCat(name){
+    const copy =cats.slice()
+    copy.shift(name)
+    return copy
+}
+
